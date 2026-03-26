444e22111908471d01a56a98c7c55ac0a0f68734
wvkizvshi <ysk.u.xcviii@gmail.com>
2026-03-26 18:23:46 +0900

    add: PrimeReact, normalize.cssを導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import 'normalize.css'
+import 'primeicons/primeicons.css'
+import 'primereact/resources/primereact.min.css'
+import 'primereact/resources/themes/mira/theme.css'
 
 import '@/app/globals.css'
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Button } from 'primereact/button'
 
 export const Home = () => {
   return (
@@ -9,10 +10,15 @@ export const Home = () => {
         alignItems: 'center',
         justifyContent: 'center',
         height: '100vh',
+        gap: '1rem',
       }}
     >
       <Image src="/logo.png" alt="Resume Builder" width={200} height={200} />
       <span>🚧 Under construction... 🚧</span>
+      <div style={{ display: 'flex', gap: '0.75rem' }}>
+        <Button label="Mira Button" icon="pi pi-check" />
+        <Button label="Secondary" severity="secondary" outlined />
+      </div>
     </div>
   )
 }
