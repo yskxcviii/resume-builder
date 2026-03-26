1c5a6000bd1b03dfe167f32547c10671cc0485a8
wvkizvshi <ysk.u.xcviii@gmail.com>
2026-03-26 16:29:15 +0900

    add: Next.jsの雛形追加

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import tseslint from 'typescript-eslint'
+
+const eslintConfig = defineConfig([
+  { ignores: ['node_modules', '.next', 'out', 'build', 'dist', 'coverage'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...nextVitals,
+  ...nextTs,
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'no-relative-import-paths': noRelativeImportPaths,
+    },
+    rules: {
+      'no-relative-import-paths/no-relative-import-paths': [
+        'error',
+        { allowSameFolder: false, rootDir: 'src', prefix: '@' },
+      ],
+    },
+  },
+]);
+
+export default eslintConfig;
